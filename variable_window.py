e7dbda57c8c23c2a5b0d276f6bcd31d58b7c1760
Alessandro <alessandrozanni.dev@gmail.com>
2016-06-06 19:34:14 +0200

    cost cube aggregation

--- variable_window.py
+++ variable_window.py
@@ -8,10 +8,12 @@ import cv2
 import argparse
 from matplotlib import pyplot as plt
 
+NUM_DISP=32
+
 def disparity(imgL, imgR, filter_size):
     window_size = 3
-    min_disp = 16
-    num_disp = 112-min_disp
+    min_disp = 0#16
+    num_disp = NUM_DISP#112-min_disp
     block_size = 5
     stereo = cv2.StereoSGBM_create(
         minDisparity=min_disp,
@@ -26,7 +28,6 @@ def disparity(imgL, imgR, filter_size):
     print('computing disparity...')
     disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
     disparity = (disp-min_disp)/num_disp
-    print("disparity: ", disparity)
     cv2.imshow('disparity',  disparity)
     return disparity
 
@@ -37,29 +38,20 @@ def matrix_mean(matrix):
     for i in range(rows):
         for j in range(columns):
     	    sum += matrix[i][j]
-            print("sum_c:", sum)
-        print("sum_r", sum)
-    print("sum", sum)
-    print("m:", sum/(rows*columns))
     return sum/(rows*columns)
 
-def variable_window(disparity, x, y): #mappa di diparità e coordinate del punto
-    print("disparity size: ", disparity.size)
-    size=2
-    mean=0
-    for i in range(1, size, disparity.size):
-        size += 1
-        x1 = x - step
-        x2 = x + step
-        y1 = y - step
-        y2 = y + step
-        if (x1 < 0) or (x2 < 0) or (y1<0) or (y2<0): #near a border
-            return mean
-#        a=[x1:x2][y1:y2]
-        a = [[1,2,],[3,4]]
-        mean = matrix_mean(a)
-        print("mean: ", mean)
-        size += 1
+#def variable_window(disparity, x, y):
+    #mappa di diparita' e coordinate del punto
+
+def cost_map(imgL, imgR):
+    rows = len(imgL)
+    columns = len(imgL[0])
+    cost = np.zeros((rows, columns, NUM_DISP))
+    for x in range(0, rows-1):
+        for y in range(0, columns-1):
+            for d in range(0, NUM_DISP-1):
+                cost[x,y,d] = abs(imgL[x,y] - imgR[x-d,y])
+    return cost
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -72,19 +64,26 @@ if __name__ == '__main__':
 
     print('loading images...')
     # retrieve the intensity of the pixels by cv2.IMREAD_GRAYSCALE
-    imgL = cv2.pyrDown(cv2.imread('images/img_L.png'))  # downscale images for faster processing
-    imgR = cv2.pyrDown(cv2.imread('images/img_R.png'))
-
+    imgL = cv2.pyrDown(cv2.imread('images/img_L.png', cv2.IMREAD_GRAYSCALE))  # downscale images for faster processing
+    imgR = cv2.pyrDown(cv2.imread('images/img_R.png', cv2.IMREAD_GRAYSCALE))
+    print imgL[0,0]
     if imgL is not None:
         print("Shape: " + str(imgL.shape))
         print("Size: " + str(imgL.size))
         print("Type: " + str(imgL.dtype))
 #        cv2.imshow('box filt', cv2.boxFilter(imgL, cv2.CV_8U, (filter_size, filter_size)))
-        #boxFilter(imgL, int(filter_size))
 
     disparity = disparity(imgL, imgR, filter_size)
+    #variable_window(disparity, 0, 0)
 
-#    variable_window(disparity)
+    #Cost cube aggregation
+    cm = cost_map(imgL, imgR)
+    print("cost map: ", cm)
+
+    #Fixed Window
+
+    #Variable Window
+
 
     cv2.imshow('left', imgL)
     cv2.imshow('right', imgR)
